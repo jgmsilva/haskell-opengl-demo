5c1a7aefb0206a58e2b8ec373fc9944fb0315385
jgmsilva <jgui.silva1510@gmail.com>
2023-11-26 16:41:00 -0300

    end of opengltutorial1

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,26 @@
-import Graphics.Rendering.OpenGL
-import Graphics.UI.GLUT
+import           Graphics.Rendering.OpenGL
+import           Graphics.UI.GLUT
+
+
+myPoints :: [(GLfloat,GLfloat,GLfloat)]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
 
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   _window <- createWindow "Hello World"
   displayCallback $= display
+  reshapeCallback $= Just reshape
   mainLoop
 
 display :: DisplayCallback
 display = do
   clear [ ColorBuffer ]
+  renderPrimitive Triangles $
+     mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
   flush
+
+reshape :: ReshapeCallback
+reshape size = do
+  viewport $= (Position 0 0, size)
+  postRedisplay Nothing
